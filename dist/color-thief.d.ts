144d7b32aac27b18cfb125527f9e3bc961869fc0
Oscar Cuatrecasas <oscar@batou.xyz>
2024-04-17 20:28:17 +0200

    Update color-thief.d.ts

--- dist/color-thief.d.ts
+++ dist/color-thief.d.ts
@@ -1,19 +1,13 @@
 export type Palette = [red: number, green: number, blue: number];
 
 /**
- * Use the median cut algorithm provided by quantize.js to cluster similar
- * colors and return the base color from the largest cluster.
+ * Use the median cut algorithm provided by quantize.js
+ * to cluster similar colors.
  */
-export function getColor(sourceImage: HTMLImageElement, quality?: number): Palette;
+export function getColor(image: string, quality?: number): Promise<Palette>;
 
 /**
  * Use the median cut algorithm provided by quantize.js
  * to cluster similar colors.
  */
-export function getPaletteFromURL(URL: string, colorCount?: number, quality?: number): Promise<Palette[]>;
-
-/**
- * Use the median cut algorithm provided by quantize.js
- * to cluster similar colors.
- */
-export function getColorFromURL(imageURL: string, quality?: number): Promise<Palette>;
+export function getPalette(image: string, colorCount?: number, quality?: number): Promise<Palette[]>;
